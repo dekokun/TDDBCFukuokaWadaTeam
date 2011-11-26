21360417ce5c1212b214395b63807b24899e53e8
dekokun <shintaro.kurachi@gmail.com>
2011-11-26 13:38:41 +0900

    TweetCategorizer の spec と空実装を追加

---
+++ tweet_categorizer_spec.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require 'rspec'
+require File.expand_path(
+  File.join(File.dirname(__FILE__),
+      'tweet_categorizer'))
+
+describe TweetCategorizer do
+end
+
